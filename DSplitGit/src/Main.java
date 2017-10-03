36e805f32443e447ae9401529a2b3df43edb3fe2
draus daniel <danieldraus1@interia.pl>
2017-10-03 09:00:26 +0200

    some changes

---
+++ DSplitGit/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * 
+ */
+
+/**
+ * @author draus
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	final static String sLabelText = "Welcome in DSplit";
+	final static String sTitle = "DSplit By Daniel Draus";
+	
+	public static void main(String[] args) {
+		App.main(args);
+	
+	}
+
+}
